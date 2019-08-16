e9c45bddcf99e236d3554bab7e75facc085b6f69
Michael Lynch <michael@michaelrlynch.com>
2019-08-16 04:03:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map.capitalize do | ep |
+  planeteer_calls.map.titleize do | ep |
     ep + "!"
   end
 end
